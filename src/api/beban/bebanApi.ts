d84aefe37b32fd63268558bc903e2cc26a9a0f2a
Khairul Bahri <chibaxp.tech@gmail.com>
2022-10-15 07:48:40 +0700

    feat: integrate beban

---
+++ src/api/beban/bebanApi.ts
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react'
+// import { toast } from 'src/components/toast'
+import { Axios } from '../axios'
+import { Params } from '../types'
+
+interface ParamsBebanList extends Params {
+  tanggal?: string
+}
+
+const endpoint = '/beban'
+
+const bebanApi = () => {
+  const [bebanList, setBebanList] = useState<[]>([])
+  const [totalData, setTotalData] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const getBebanList = useCallback(async (params: ParamsBebanList = {}) => {
+    setLoading(true)
+
+    try {
+      const { data: { data, total } } = await Axios.get(endpoint, { params })
+      setBebanList(data || [])
+      setTotalData(total)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+
+  return {
+    bebanList,
+    loading,
+    totalData,
+    getBebanList,
+  }
+}
+
+export default bebanApi
--- src/features/beban-harian/BebanHarian.tsx
+++ src/features/beban-harian/BebanHarian.tsx
@@ -1,8 +1,10 @@
 // ** React Imports
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 
 // ** MUI Imports
-import Paper from "@mui/material/Paper";
+import { Card, CardContent, Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import Table from "@mui/material/Table";
 import TableRow from "@mui/material/TableRow";
 import TableHead from "@mui/material/TableHead";
@@ -10,83 +12,26 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TablePagination from "@mui/material/TablePagination";
+import PageHeader from "src/@core/components/page-header";
+import DownloadIcon from "src/assets/icons/download-icon.svg";
+import EditIcon from "src/assets/icons/edit-icon.svg";
 
-interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
-  label: string;
-  minWidth?: number;
-  align?: "right";
-  rowSpan?: number;
-  format?: (value: number) => string;
-}
+import { WrapperFilter } from "src/components/filter";
+import { bebanApi } from "src/api/beban";
+import {
+  Beban,
+  KategoriPembangkit,
+  Pembangkit,
+  Data,
+} from "src/api/beban/types";
+import { time } from "./BebanHarian.constant";
 
-const columns: readonly Column[] = [
-  { id: "name", label: "Jenis Pembangkit", rowSpan: 2, minWidth: 170 },
-  { id: "code", label: "Pembangkit", rowSpan: 2, minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
-  },
-];
-
-interface Data {
-  name: string;
-  code: string;
-  size: number;
-  density: number;
-  population: number;
-}
-
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
-
-const TableStickyHeader = () => {
+const BebanHarian = () => {
   // ** States
   const [page, setPage] = useState<number>(0);
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+
+  const { getBebanList, bebanList } = bebanApi();
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -97,196 +42,145 @@ const TableStickyHeader = () => {
     setPage(0);
   };
 
-  const TestTable = () => {
-    return (
-      <>
-        <TableRow>
-          <TableCell
-            sx={{
-              background: "#5459D1",
-              color: "#FFFFFF",
-              fontSize: "14px",
-              fontWeight: "700 !important",
-            }}
-            colSpan={8}
-          >
-            Subsistem Tanjungjati
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>PLTA</TableCell>
-          <TableCell>Jelok</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>PLTA</TableCell>
-          <TableCell>Timo</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>PLTA</TableCell>
-          <TableCell>Kedung Ombo</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-          <TableCell align="center">6</TableCell>
-        </TableRow>
-        <TableRow
-          sx={{
-            background:
-              "linear-gradient(0deg, rgba(255, 255, 255, 0.88), rgba(255, 255, 255, 0.88)), #26C6F9;",
-          }}
-        >
-          <TableCell colSpan={2}>Total Non Mrica</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-        </TableRow>
-        <TableRow
-          sx={{
-            background:
-              "linear-gradient(0deg, rgba(255, 255, 255, 0.88), rgba(255, 255, 255, 0.88)), #6D788D;",
-          }}
-        >
-          <TableCell colSpan={2}>Total Hidro</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-          <TableCell align="center">218</TableCell>
-        </TableRow>
-      </>
-    );
-  };
+  useEffect(() => {
+    getBebanList({ tanggal: "2022-10-13" });
+  }, []);
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer>
-        <Table>
-          <TableHead sx={{ height: "30px" }}>
-            <TableRow>
-              <TableCell
-                sx={{ backgroundColoe: "red" }}
-                size="small"
-                rowSpan={2}
-              >
-                Jenis Pembangkit
-              </TableCell>
-              <TableCell size="small" rowSpan={2}>
-                Pembangkit
-              </TableCell>
-              <TableCell size="small" align="center" colSpan={2}>
-                00.30
-              </TableCell>
-              <TableCell size="small" align="center" colSpan={2}>
-                00.30
-              </TableCell>
-              <TableCell size="small" align="center" colSpan={2}>
-                00.30
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" size="small">
-                MV
-              </TableCell>
-              <TableCell align="center" size="small">
-                MX
-              </TableCell>
-              <TableCell align="center" size="small">
-                MV
-              </TableCell>
-              <TableCell align="center" size="small">
-                MX
-              </TableCell>
-              <TableCell align="center" size="small">
-                MV
-              </TableCell>
-              <TableCell align="center" size="small">
-                MX
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TestTable />
-            <TestTable />
-            <TestTable />
-            <TestTable />
-          </TableBody>
-          {/* <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  rowSpan={column.rowSpan}
-                  align={column.align}
-                  sx={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow> */}
-          {/* <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  colSpan={column.rowSpan}
-                  align={column.align}
-                  sx={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow> */}
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <PageHeader
+          title={<Typography variant="h5">Beban Harian</Typography>}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent>
+            <WrapperFilter sx={{ alignItems: "baseline" }}>
+              <Typography variant="h6">Daftar File Laporan</Typography>
 
-          {/* <TableBody> */}
-          {/* {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
+              <div style={{ display: "flex", gap: "10px" }}>
+                <Button sx={{ mb: 2 }} variant="outlined">
+                  <EditIcon />
+                  Ubah Data
+                </Button>
+                <Button sx={{ mb: 2 }} variant="outlined">
+                  Set
+                </Button>
+                <Button sx={{ mb: 2 }} variant="contained">
+                  <DownloadIcon />
+                  Download laporan
+                </Button>
+              </div>
+            </WrapperFilter>
+            <TableContainer>
+              <Table>
+                <TableHead sx={{ height: "30px", background: "#F5F5F7" }}>
+                  <TableRow>
+                    <TableCell
+                      sx={{ backgroundColoe: "red" }}
+                      size="small"
+                      rowSpan={2}
+                    >
+                      Jenis Pembangkit
+                    </TableCell>
+                    <TableCell size="small" rowSpan={2}>
+                      Pembangkit
+                    </TableCell>
+                    {time.map((value) => (
+                      <TableCell size="small" align="center" colSpan={2}>
+                        {value}
+                      </TableCell>
+                    ))}
                   </TableRow>
-                );
-              })} */}
-          {/* </TableBody> */}
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+                  <TableRow>
+                    {time.map(() => (
+                      <>
+                        <TableCell>MW</TableCell>
+                        <TableCell>MX</TableCell>
+                      </>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {bebanList.map((value: Beban) => {
+                    return (
+                      <>
+                        <TableRow>
+                          <TableCell
+                            sx={{
+                              background: value?.color,
+                              color: "#FFFFFF",
+                              fontSize: "14px",
+                              fontWeight: "700 !important",
+                            }}
+                            colSpan={time.length * 2 + 2}
+                          >
+                            {value?.sub_sistem}
+                          </TableCell>
+                        </TableRow>
+                        {value?.kategori_pembangkit.map(
+                          (kategori_pembangkit: KategoriPembangkit) => (
+                            <>
+                              {kategori_pembangkit.pembangkit.map(
+                                (pembangkit: Pembangkit) => {
+                                  console.log(
+                                    kategori_pembangkit.nama,
+                                    pembangkit
+                                  );
+                                  return (
+                                    <TableRow>
+                                      <TableCell>
+                                        {kategori_pembangkit.nama}
+                                      </TableCell>
+                                      <TableCell>{pembangkit.nama}</TableCell>
+                                      {Object.values(time).map((value) => {
+                                        const mw =
+                                          "mw_" + value.replace(".", "");
+                                        const mx =
+                                          "mx_" + value.replace(".", "");
+                                        return (
+                                          <>
+                                            <TableCell>
+                                              {pembangkit?.data[mw]!}
+                                            </TableCell>
+                                            <TableCell>
+                                              {pembangkit?.data[mx]!}
+                                            </TableCell>
+                                          </>
+                                        );
+                                      })}
+                                    </TableRow>
+                                  );
+                                }
+                              )}
+                            </>
+                          )
+                        )}
+                      </>
+                    );
+                  })}
+                  {/* <TestTable />
+                  <TestTable />
+                  <TestTable />
+                  <TestTable /> */}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={12}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
-export default TableStickyHeader;
+export default BebanHarian;
