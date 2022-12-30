dab8f379e308ed34f1382ae7d075a40512364460
rian3005 <riannazar03@gmail.com>
2022-12-30 10:09:20 +0700

    commit sementara

--- src/api/subsistem/subsistemApi.ts
+++ src/api/subsistem/subsistemApi.ts
@@ -1,76 +1,77 @@
-import { useCallback, useState } from 'react'
-import { toast } from 'src/components/toast'
-import { Axios } from '../axios'
-import { Params } from '../types'
+import { useCallback, useState } from "react";
+import { toast } from "src/components/toast";
+import { Axios } from "../axios";
+import { Params } from "../types";
 
-const endpoint = '/sub-sistem'
+const endpoint = "/sub-sistem";
 
 const subsistemApi = () => {
-  const [subsistemList, setSubsistemList] = useState<[]>([])
-  const [totalData, setTotalData] = useState<number>(0)
+  const [subsistemList, setSubsistemList] = useState<[]>([]);
+  const [totalData, setTotalData] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
 
   const getSubsistemList = useCallback(async (params: Params = {}) => {
-    setLoading(true)
+    setLoading(true);
 
     try {
-      const { data: { data, total } } = await Axios.get(endpoint, { params })
-      setSubsistemList(data || [])
-      setTotalData(total)
+      const {
+        data: { data, total },
+      } = await Axios.get(endpoint, { params });
+      setSubsistemList(data || []);
+      setTotalData(total);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   const getSubsistemDetail = useCallback(async (id: String) => {
-    setLoading(true)
+    setLoading(true);
 
     try {
-      const { data } = await Axios.get(`${endpoint}/${id}`)
-      return data
+      const { data } = await Axios.get(`${endpoint}/${id}`);
+      return data;
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   const createSubsistem = useCallback(async (payload: any) => {
-    setLoading(true)
+    setLoading(true);
 
     try {
-      await Axios.post(endpoint, payload)
-      toast.success('Berhasil menambahkan subsistem')
+      await Axios.post(endpoint, payload);
+      toast.success("Berhasil menambahkan subsistem");
     } catch (error) {
-      toast.error('Gagal menambahkan subsistem')
+      toast.error("Gagal menambahkan subsistem");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   const updateSubsistem = useCallback(async (payload: any) => {
-    setLoading(true)
+    setLoading(true);
 
     try {
-      await Axios.put(endpoint, payload)
-      toast.success('Berhasil mengubah subsistem')
+      await Axios.put(endpoint, payload);
+      toast.success("Berhasil mengubah subsistem");
     } catch (error) {
-      toast.error('Gagal mengubah subsistem')
+      toast.error("Gagal mengubah subsistem");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   const deleteSubsistem = useCallback(async (payload: any) => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await Axios.delete(endpoint, { data: payload })
-      toast.success('Berhasil menghapus subsistem')
+      await Axios.delete(endpoint, { data: payload });
+      toast.success("Berhasil menghapus subsistem");
     } catch (error) {
-      toast.error('Gagal menghapus subsistem')
+      toast.error("Gagal menghapus subsistem");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
-
+  }, []);
 
   return {
     subsistemList,
@@ -80,8 +81,8 @@ const subsistemApi = () => {
     getSubsistemDetail,
     createSubsistem,
     updateSubsistem,
-    deleteSubsistem
-  }
-}
+    deleteSubsistem,
+  };
+};
 
-export default subsistemApi
+export default subsistemApi;
